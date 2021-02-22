5edcf4816431b06f5e683718f59dbd1b0d777dc1
Fidel Jeldres <fjeldres@gmail.com>
2021-02-22 16:34:36 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
+import { ContadorModule } from './contador/contador.nodule';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
